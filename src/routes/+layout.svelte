36e907387c4823e90cf5dd3494132825f182620e
Martin Gabel <martin.gabel@becker-antriebe.com>
2024-02-21 13:49:08 +0100

    roboto, material-icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
 	import { base } from '$app/paths';
+	import '@fontsource/roboto';
+	import '@fontsource/material-icons';
 </script>
 
 <nav>
+	<span class="material-icons">face</span>
 	<a href="{base}/">Home</a>
 	<a href="{base}/about">About</a>
 </nav>
@@ -13,15 +16,41 @@
 		background-color: #f2eee2;
 		color: #0084f6;
 		transition: background-color 0.3s;
+		font-family: 'Roboto', sans-serif;
 	}
 
 	:global(body.dark-mode) {
 		background-color: #1d3040;
 		color: #bfc2c7;
 	}
+
 	nav a {
 		color: #acb3be;
 		text-decoration: none;
 		font-weight: bold;
+	}
+	:global(.material-icons) {
+		font-family: 'Material Icons';
+		font-weight: normal;
+		font-style: normal;
+		font-size: 24px; /* Preferred icon size */
+		display: inline-block;
+		line-height: 1;
+		text-transform: none;
+		letter-spacing: normal;
+		word-wrap: normal;
+		white-space: nowrap;
+		direction: ltr;
+
+		/* Support for all WebKit browsers. */
+		-webkit-font-smoothing: antialiased;
+		/* Support for Safari and Chrome. */
+		text-rendering: optimizeLegibility;
+
+		/* Support for Firefox. */
+		-moz-osx-font-smoothing: grayscale;
+
+		/* Support for IE. */
+		font-feature-settings: 'liga';
 	}
 </style>
